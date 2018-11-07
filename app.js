c52a1b1754e84ffe21e6d40ee5caf71c3d8e1a64
jason <jf7776@gmail.com>
2018-11-07 15:15:07 -0800

    postgres

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3005;
 var bodyParser = require("body-parser");
 
 app.use(bodyParser.json());
--- router/routes.js
+++ router/routes.js
@@ -1,23 +1,87 @@
 const express = require("express");
 var router = express.Router();
+var pg = require("pg");
+var connectionString = "postgres://" + "304group" + ":" + "rohan" +  "@localhost:" + "5433" + "/marketWatch";
+var client = new pg.Client(connectionString);
+client.connect();
 
+let price = `CREATE TABLE IF NOT EXISTS price(priceID VARCHAR(10) NOT NULL PRIMARY KEY, pDate INTEGER, pTime INTEGER, value INTEGER)`;
+let stock = `CREATE TABLE IF NOT EXISTS company(companyID VARCHAR(4) NOT NULL PRIMARY KEY, numOfShares INTEGER, industry VARCHAR(32), companyName VARCHAR(32), priceID VARCHAR(10) NOT NULL, FOREIGN KEY (priceID) REFERENCES price(priceID))`;
 
+let arr = [price, stock];
 
-router.get("/", (req, res) => {
-    // homepage
+arr.forEach((query) => {
+    client.query(query, (err, result) => {
+        if (err) {
+            console.log(query + err);
+        } else {
+            console.log(result);
+        }
+    })
+});
+
+let addQuery = `INSERT INTO company(companyID, numOfShares, industry, companyName, priceID) values($1, $2, $3, $4, $5)`;
+let addPrice = `INSERT INTO price(priceID, pDate, pTime, value) values($1, $2, $3, $4)`;
+
+client.query(addPrice, ["123", 7, 4, 58], (err, result) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(result);
+    }
+});
+
+client.query(addPrice, ["1279", 5, 8, 150], (err, result) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(result);
+    }
+});
+
+client.query(addQuery, ["APPL", 50, "Tech", "Apple", "123"], (err, result) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(result);
+    }
+});
+
+client.query(addQuery, ["GOOG", 25, "Tech", "Google", "1279"], (err, result) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(result);
+    }
 });
 
 router.get("/buy", (req, res) => {
     console.log("/buy called");
 });
 
+router.get("/getPrice", (req, res) => {
+    let select = `SELECT * FROM price`;
+    client.query(select, (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.send(result);
+        }
+    });
+});
+
+router.get("/getCompany", (req, res) => {
+    let select = `SELECT * FROM company`;
+    client.query(select, (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.send(result);
+        }
+    });
+});
+
 module.exports = router;
-
-
-// var pg = require("pg");
-// var connectionString = "postgres://" + "304group" + ":" + "rohan" +  "@localhost:" + "5433" + "/marketWatch";
-// var client = new pg.Client(connectionString);
-// client.connect();
 
 
 // let leaderBoard = `CREATE TABLE IF NOT EXISTS LeaderBoard(LeaderBoardID VARCHAR(10) NOT NULL, numOfTraders INTEGER)`;
