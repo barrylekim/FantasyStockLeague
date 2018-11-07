fa1d1a004f4ba16606b473ef0a4b8262703d8fa4
jason <jf7776@gmail.com>
2018-11-07 13:25:00 -0800

    create table

--- router/routes.js
+++ router/routes.js
@@ -1,18 +1,34 @@
 const express = require("express");
 var router = express.Router();
 var pg = require("pg");
-var connectionString = "postgres://" + "postgres" + ":" + "temp123" +  "@localhost:" + "5433" + "/postgres";
+var connectionString = "postgres://" + "304group" + ":" + "rohan" +  "@localhost:" + "5433" + "/marketWatch";
 var client = new pg.Client(connectionString);
 client.connect();
 
-let createTableQuery = `CREATE TABLE IF NOT EXISTS users(email VARCHAR(40) UNIQUE NOT NULL, password VARCHAR(40) NOT NULL, firstName VARCHAR(40) NOT NULL, lastName VARCHAR(40) NOT NULL)`;
-client.query(createTableQuery , (err, res) => {
+let leaderBoard = `CREATE TABLE IF NOT EXISTS LeaderBoard(LeaderBoardID VARCHAR(10) NOT NULL, numOfTraders INTEGER)`;
+let trader = `CREATE TABLE IF NOT EXISTS trader(traderID VARHCAR(10) NOT NULL funds MONEY, tradername VARCHAR(12) UNIQUE, portfolioid`
+let isOn = `CREATE TABLE IF NOT EXISTS ison(leaderboardid VARCHAR(10) NOT NULL, traderid VARCHAR(10) NOT NULL, rank INTEGER, PRIMARY KEY (leaderboardid, traderid), FOREIGN KEY (leaderboardID) REFERENCES leaderboard(leaderboardID), FOREIGN KEY (traderid) REFERENCES trader(traderID))`;
+client.query(leaderBoard , (err, res) => {
     if (err) {
         console.log("ERROR: " + err);
     } else {
         console.log("User table created");
     }
 });
+
+client.query(isOn, (err, res) =>{
+    console.log(err);
+    console.log(res);
+})
+
+let select = `SELECT * FROM leaderBoard`;
+client.query(select, (err, res) => {
+    if (err) {
+
+    } else {
+        console.log(res);
+    }
+})
 
 //connection.query("SELECT * from temp", function( err, results, fields) {})
 // router.get("/", (req, res) => {
