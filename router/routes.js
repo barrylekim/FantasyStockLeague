02d3e6f46490344333b12b9654563cfc7af2861e
jason <jf7776@gmail.com>
2018-11-07 14:54:13 -0800

    express

--- router/routes.js
+++ router/routes.js
@@ -1,37 +1,52 @@
 const express = require("express");
 var router = express.Router();
-var pg = require("pg");
-var connectionString = "postgres://" + "304group" + ":" + "rohan" +  "@localhost:" + "5433" + "/marketWatch";
-var client = new pg.Client(connectionString);
-client.connect();
 
-let leaderBoard = `CREATE TABLE IF NOT EXISTS LeaderBoard(LeaderBoardID VARCHAR(10) NOT NULL, numOfTraders INTEGER)`;
-let trader = `CREATE TABLE IF NOT EXISTS trader(traderID VARHCAR(10) NOT NULL funds MONEY, tradername VARCHAR(12) UNIQUE, portfolioid`
-let isOn = `CREATE TABLE IF NOT EXISTS ison(leaderboardid VARCHAR(10) NOT NULL, traderid VARCHAR(10) NOT NULL, rank INTEGER, PRIMARY KEY (leaderboardid, traderid), FOREIGN KEY (leaderboardID) REFERENCES leaderboard(leaderboardID), FOREIGN KEY (traderid) REFERENCES trader(traderID))`;
-client.query(leaderBoard , (err, res) => {
-    if (err) {
-        console.log("ERROR: " + err);
-    } else {
-        console.log("User table created");
-    }
+
+
+router.get("/", (req, res) => {
+    // homepage
 });
 
-client.query(isOn, (err, res) =>{
-    console.log(err);
-    console.log(res);
-})
-
-let select = `SELECT * FROM leaderBoard`;
-client.query(select, (err, res) => {
-    if (err) {
-
-    } else {
-        console.log(res);
-    }
-})
-
-//connection.query("SELECT * from temp", function( err, results, fields) {})
-// router.get("/", (req, res) => {
-// });
+router.get("/buy", (req, res) => {
+    console.log("/buy called");
+});
 
 module.exports = router;
+
+
+// var pg = require("pg");
+// var connectionString = "postgres://" + "304group" + ":" + "rohan" +  "@localhost:" + "5433" + "/marketWatch";
+// var client = new pg.Client(connectionString);
+// client.connect();
+
+
+// let leaderBoard = `CREATE TABLE IF NOT EXISTS LeaderBoard(LeaderBoardID VARCHAR(10) NOT NULL, numOfTraders INTEGER)`;
+// let trader = `CREATE TABLE IF NOT EXISTS trader(traderID VARHCAR(10) NOT NULL funds MONEY, tradername VARCHAR(12) UNIQUE, portfolioid`
+// let isOn = `CREATE TABLE IF NOT EXISTS ison(leaderboardid VARCHAR(10) NOT NULL, traderid VARCHAR(10) NOT NULL, rank INTEGER, PRIMARY KEY (leaderboardid, traderid), FOREIGN KEY (leaderboardID) REFERENCES leaderboard(leaderboardID), FOREIGN KEY (traderid) REFERENCES trader(traderID))`;
+
+// let arr = [leaderBoard, trader, isOn]; 
+// client.query(leaderBoard , (err, res) => {
+//     if (err) {
+//         console.log("ERROR: " + err);
+//     } else {
+//         console.log("User table created");
+//     }
+// });
+
+// client.query(isOn, (err, res) =>{
+//     console.log(err);
+//     console.log(res);
+// })
+
+// let select = `SELECT * FROM leaderBoard`;
+// client.query(select, (err, res) => {
+//     if (err) {
+
+//     } else {
+//         console.log(res);
+//     }
+// });
+
+// //connection.query("SELECT * from temp", function( err, results, fields) {})
+// // router.get("/", (req, res) => {
+// // });
