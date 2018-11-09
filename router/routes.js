7c5ec488fed5951e1d3d9428fa74c1943f8be4b9
jason <jf7776@gmail.com>
2018-11-09 10:32:20 -0800

    tables

--- router/routes.js
+++ router/routes.js
@@ -5,10 +5,18 @@ var connectionString = "postgres://" + "304group" + ":" + "rohan" +  "@localhost
 var client = new pg.Client(connectionString);
 client.connect();
 
-let price = `CREATE TABLE IF NOT EXISTS price(priceID VARCHAR(10) NOT NULL PRIMARY KEY, pDate INTEGER, pTime INTEGER, value INTEGER)`;
-let stock = `CREATE TABLE IF NOT EXISTS company(companyID VARCHAR(4) NOT NULL PRIMARY KEY, numOfShares INTEGER, industry VARCHAR(32), companyName VARCHAR(32), priceID VARCHAR(10) NOT NULL, FOREIGN KEY (priceID) REFERENCES price(priceID))`;
+let price = `CREATE TABLE IF NOT EXISTS price(priceID CHAR(10) NOT NULL PRIMARY KEY, pDate INTEGER, pTime INTEGER, value INTEGER)`;
+let stock = `CREATE TABLE IF NOT EXISTS company(companyID CHAR(4) NOT NULL PRIMARY KEY, numOfShares INTEGER, industry VARCHAR(32), companyName VARCHAR(32), priceID CHAR(10) NOT NULL, FOREIGN KEY (priceID) REFERENCES price(priceID))`;
+let leaderBoard = `CREATE TABLE IF NOT EXISTS leaderboard(leaderboardID CHAR(10) NOT NULL PRIMARY KEY, numOfTraders INTEGER)`;
+let isOn = `CREATE TABLE IF NOT EXISTS ison(leaderboardID CHAR(10) NOT NULL, traderID CHAR(10) NOT NULL, rank INTEGER, PRIMARY KEY (leaderboardID, traderID), FOREIGN KEY (leaderboardID) REFERENCES leaderboard(leaderboardID), FOREIGN KEY (traderID) REFERENCES trader(traderID))`;
+let trader = `CREATE TABLE IF NOT EXISTS trader(traderID CHAR(10) NOT NULL PRIMARY KEY, funds MONEY, traderName VARCHAR(12) UNIQUE, portfolioID INTEGER NOT NULL, FOREIGN KEY (portfolioID) REFERENCES portfolio ON DELETE CASCADE ON UPDATE CASCADE)`;
+let portfolio = `CREATE TABLE IF NOT EXISTS portfolio(portfolioID INTEGER NOT NULL PRIMARY KEY)`;
+let watchList = `CREATE TABLE IF NOT EXISTS watchlist(watchlistID INTEGER NOT NULL PRIMARY KEY, traderID CHAR(10), FOREIGN KEY (traderID) REFERENCES trader ON DELETE SET NULL ON UPDATE CASCADE)`;
+let includes = `CREATE TABLE IF NOT EXISTS includes(watchlistID INTEGER NOT NULL, companyID CHAR(4) NOT NUlL, PRIMARY KEY (companyID, watchlistID), FOREIGN KEY (watchlistID) REFERENCES watchlist(watchlistID), FOREIGN KEY (companyID) REFERENCES company(companyID))`;
+let contains = `CREATE TABLE IF NOT EXISTS contains(portfolioID INTEGER NOT NULL, companyID CHAR(4), PRIMARY KEY (portfolioID, companyID), FOREIGN KEY (portfolioID) REFERENCES portfolio(portfolioID), FOREIGN KEY (companyID) REFERENCES company(companyID))`;
+let transaction = `CREATE TABLE IF NOT EXISTS transaction(transactionID CHAR(10) PRIMARY KEY, traderID CHAR(10) NOT NULL, companyID CHAR(4) NOT NULL, priceID CHAR(10) NOT NULL, type BIT, sharesPurchased INTEGER, FOREIGN KEY (traderID) REFERENCES trader(traderID) ON DELETE NO ACTION ON UPDATE CASCADE, FOREIGN KEY (priceID) REFERENCES price(priceID) ON DELETE NO ACTION ON UPDATE CASCADE)`;
 
-let arr = [price, stock];
+let arr = [price, stock, leaderBoard, isOn, portfolio, trader, watchList, includes, contains, transaction];
 
 arr.forEach((query) => {
     client.query(query, (err, result) => {
@@ -18,41 +26,6 @@ arr.forEach((query) => {
             console.log(result);
         }
     })
-});
-
-let addQuery = `INSERT INTO company(companyID, numOfShares, industry, companyName, priceID) values($1, $2, $3, $4, $5)`;
-let addPrice = `INSERT INTO price(priceID, pDate, pTime, value) values($1, $2, $3, $4)`;
-
-client.query(addPrice, ["123", 7, 4, 58], (err, result) => {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log(result);
-    }
-});
-
-client.query(addPrice, ["1279", 5, 8, 150], (err, result) => {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log(result);
-    }
-});
-
-client.query(addQuery, ["APPL", 50, "Tech", "Apple", "123"], (err, result) => {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log(result);
-    }
-});
-
-client.query(addQuery, ["GOOG", 25, "Tech", "Google", "1279"], (err, result) => {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log(result);
-    }
 });
 
 router.get("/buy", (req, res) => {
@@ -65,7 +38,7 @@ router.get("/getPrice", (req, res) => {
         if (err) {
             console.log(err);
         } else {
-            res.send(result);
+            res.send(result.rows);
         }
     });
 });
@@ -76,17 +49,49 @@ router.get("/getCompany", (req, res) => {
         if (err) {
             console.log(err);
         } else {
-            res.send(result);
+            res.send(result.rows);
         }
     });
 });
 
+router.get("/addRows" , (req, res) => {
+    let addQuery = `INSERT INTO company(companyID, numOfShares, industry, companyName, priceID) values($1, $2, $3, $4, $5)`;
+    let addPrice = `INSERT INTO price(priceID, pDate, pTime, value) values($1, $2, $3, $4)`;
+    
+    client.query(addPrice, ["123", 7, 4, 58], (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(result);
+        }
+    });
+    
+    client.query(addPrice, ["1279", 5, 8, 150], (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(result);
+        }
+    });
+    
+    client.query(addQuery, ["APPL", 50, "Tech", "Apple", "123"], (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(result);
+        }
+    });
+    
+    client.query(addQuery, ["GOOG", 25, "Tech", "Google", "1279"], (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(result);
+        }
+    });    
+});
+
 module.exports = router;
-
-
-// let leaderBoard = `CREATE TABLE IF NOT EXISTS LeaderBoard(LeaderBoardID VARCHAR(10) NOT NULL, numOfTraders INTEGER)`;
-// let trader = `CREATE TABLE IF NOT EXISTS trader(traderID VARHCAR(10) NOT NULL funds MONEY, tradername VARCHAR(12) UNIQUE, portfolioid`
-// let isOn = `CREATE TABLE IF NOT EXISTS ison(leaderboardid VARCHAR(10) NOT NULL, traderid VARCHAR(10) NOT NULL, rank INTEGER, PRIMARY KEY (leaderboardid, traderid), FOREIGN KEY (leaderboardID) REFERENCES leaderboard(leaderboardID), FOREIGN KEY (traderid) REFERENCES trader(traderID))`;
 
 // let arr = [leaderBoard, trader, isOn]; 
 // client.query(leaderBoard , (err, res) => {
