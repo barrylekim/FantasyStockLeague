1c61b962c6b1d76c9d64b8d704b7870fe0fa3986
rohan garg <rohangarg1998s@gmail.com>
2018-11-13 17:22:01 -0800

    lol

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
-import Watchlist from './containers/WatchList/Watchlist';
 import Leaderboard from './containers/LeaderBoard/LeaderBoard'
 import User from './components/User/User'
-import StockList from './containers/stockList/StockList'
-import Portfolio from './containers/Portfolio/Portfolio'
+import JasonContainer from './containers/JasonContainer/JasonContainer'
 //"postgres://" + "rohan" + ":" + "jason" +  "@localhost:" + "5432" + "/304"
 class App extends Component {
   constructor(props){
@@ -22,8 +20,7 @@ class App extends Component {
     this.submitData = this.submitData.bind(this); // <-- add this line
     this.handleChange = this.handleChange.bind(this); 
     this.signupData = this.signupData.bind(this);
-    this.submitDataBuy = this.submitDataBuy.bind(this);
-    this.handleChangeBuy = this.handleChangeBuy.bind(this); 
+     
   }
  
 
@@ -46,105 +43,54 @@ class App extends Component {
       }
       
     }
-submitData(event){
-  event.preventDefault();
-  let st = this.state;
-    fetch('http://localhost:3005/getTrader/'+st.value).then(res2 =>{
-      console.log(res2);
-      if(res2.status===500){
-        console.log(res2); 
-      }
-      else
-      {return res2.json();}
-    }).then(myJ=>{
-      console.log(myJ);
-        st.id = myJ.trader.traderid;
-        st.Portfolio=myJ.portfolio;
-        st.funds = myJ.trader.funds;
-        st.watchlist = myJ.watchlist;
-        let sum = 0; 
-        myJ.portfolio.forEach(el =>{
-          console.log(el);
-          let r = parseInt(el.value,10)*parseInt(el.numofshares,10);
-          sum = sum+ r; 
-        })
-        console.log(sum);
-        st.worth = sum; 
-  this.setState(st);
-        console.log('acceptedID');
-    }) 
-}
-signupData(event){
-  event.preventDefault()
-  let st = this.state
-  let data = st.value;
-  fetch('http://localhost:3005/addTrader',{
-    method: 'POST',
-    mode: 'cors',
-    credentials: 'same-origin',
-    headers:{
-      "Content-Type": "application/json; charset=utf-8"
-    },
-    body: JSON.stringify({name: data}),
-  }).then(res =>{
-    console.log(res);
-    return res.json()
-  }).then(myJ=>{
-    console.log(myJ); 
-  })
-
-}
-submitDataBuy(event){
-  event.preventDefault();
-  let comp = this.state.query;
-  let shares = this.state.queryP; 
-  let id = this.state.id; 
-  let data = {
-    traderID: id,
-    companyID: comp,
-    numOfShares: shares
-  }; 
-  fetch('http://localhost:3005/buy',{
-  method: 'POST',
-  mode: 'cors',
-  credentials: 'same-origin',
-  headers:{
-    "Content-Type": "application/json; charset=utf-8"
-  },
-  body: JSON.stringify(data),
-}).then(res =>{
-  console.log(res);
-  return res.json()
-}).then(myt=>{
-  console.log(myt);
-  let st = this.state;
-  fetch('http://localhost:3005/getTrader/'+st.value).then(res2 =>{
-    console.log(res2);
-    if(res2.status===500){
-      console.log(res2); 
+    submitData(event){
+      event.preventDefault();
+      let st = this.state;
+        fetch('http://localhost:3005/getTrader/'+st.value).then(res2 =>{
+          console.log(res2);
+          if(res2.status===500){
+            console.log(res2); 
+          }
+          else
+          {return res2.json();}
+        }).then(myJ=>{
+          console.log(myJ);
+            st.id = myJ.trader.traderid;
+            st.Portfolio=myJ.portfolio;
+            st.funds = myJ.trader.funds;
+            st.watchlist = myJ.watchlist;
+            let sum = 0; 
+            myJ.portfolio.forEach(el =>{
+              console.log(el);
+              let r = parseInt(el.value,10)*parseInt(el.numofshares,10);
+              sum = sum+ r; 
+            })
+            console.log(sum);
+            st.worth = sum; 
+      this.setState(st);
+            console.log('acceptedID');
+        }) 
     }
-    else
-    {return res2.json();}
-  }).then(myJ=>{
-    console.log(myJ);
-      st.id = myJ.trader.traderid;
-      st.funds = myJ.trader.funds;
-      st.watchlist = myJ.watchlist;
-      let sum = 0; 
-      console.log(st.Portfolio);
-      myJ.portfolio.forEach(el =>{
-        console.log(el);
-        let r = parseInt(el.value,10)*parseInt(el.numofshares,10);
-        sum = sum+ r; 
+    signupData(event){
+      event.preventDefault()
+      let st = this.state
+      let data = st.value;
+      fetch('http://localhost:3005/addTrader',{
+        method: 'POST',
+        mode: 'cors',
+        credentials: 'same-origin',
+        headers:{
+          "Content-Type": "application/json; charset=utf-8"
+        },
+        body: JSON.stringify({name: data}),
+      }).then(res =>{
+        console.log(res);
+        return res.json()
+      }).then(myJ=>{
+        console.log(myJ); 
       })
-      console.log(sum);
-      st.worth = sum; 
-this.setState(st);
-  }) 
-  
-})
-
-}
+    
+    }
   render() {
 
     return (
@@ -156,20 +102,9 @@ this.setState(st);
               <input class='email'type="name" value={this.state.value} onChange={this.handleChange.bind(this)} />
        <a href="#"><div onClick={this.submitData} class="btn">Sign In</div></a> 
 <a href="#"><div onClick={this.signupData} id="btn2">Sign Up</div></a> </div></form>: 
-[ <User name={this.state.value}worth={this.state.worth} cash= {this.state.funds}/>, 
-<form onSubmit={this.submitDataBuy.bind(this)}> 
-     <label>
-       Buy: 
-    <input   onChange={(e)=>this.handleChangeBuy("comp",e)} value = {this.state.query}/>
-    NumberOfShares:
-    <input onChange={(e)=>this.handleChangeBuy("price",e)} value={this.state.queryP}/>  
-    <input type="submit" value="Submit"/>
-    </label>            
-    </form>,
-    <Portfolio stocks = {this.state.Portfolio}></Portfolio>,
-    <StockList id={this.state.user} portfolio={this.state.Portfolio}></StockList>,
-    <Leaderboard/>,
-    <Watchlist name={this.state.value}/>]}
+[   <Leaderboard/>,
+    <JasonContainer worth={this.state.worth} funds={this.state.funds}Portfolio={this.state.Portfolio} name={this.state.value} id={this.state.id} />
+   ]}
         <header className="App-header">
           <p>
             StockWatch
--- FrontEnd/src/containers/Portfolio/Portfolio.js
+++ FrontEnd/src/containers/Portfolio/Portfolio.js
@@ -5,11 +5,73 @@
          super(props);
          console.log(props);
          this.state = {
-             stocks: props.stocks
+             stocks: props.stocks,
+             query: "",
          }
      }
-    
-     
+     handleChangeBuy(id,event) {
+        let curr = this.state;
+        if(id==="comp"){
+          curr.query = event.target.value; 
+          this.setState({curr});
+        }
+        else if(id==="price"){
+          curr.queryP = event.target.value;
+          this.setState({curr});
+        }
+        
+      }
+     submitDataBuy(event){
+        event.preventDefault();
+        let comp = this.state.query;
+        let shares = this.state.queryP; 
+        let id = this.state.id; 
+        let data = {
+          traderID: id,
+          companyID: comp,
+          numOfShares: shares
+        }; 
+        fetch('http://localhost:3005/buy',{
+        method: 'POST',
+        mode: 'cors',
+        credentials: 'same-origin',
+        headers:{
+          "Content-Type": "application/json; charset=utf-8"
+        },
+        body: JSON.stringify(data),
+      }).then(res =>{
+        console.log(res);
+        return res.json()
+      }).then(myt=>{
+        console.log(myt);
+        let st = this.state;
+        fetch('http://localhost:3005/getTrader/'+st.value).then(res2 =>{
+          console.log(res2);
+          if(res2.status===500){
+            console.log(res2); 
+          }
+          else
+          {return res2.json();}
+        }).then(myJ=>{
+          console.log(myJ);
+            st.id = myJ.trader.traderid;
+            st.funds = myJ.trader.funds;
+            st.watchlist = myJ.watchlist;
+            let sum = 0; 
+            console.log(st.Portfolio);
+            myJ.portfolio.forEach(el =>{
+              console.log(el);
+              let r = parseInt(el.value,10)*parseInt(el.numofshares,10);
+              sum = sum+ r; 
+            })
+            console.log(sum);
+            st.worth = sum; 
+      this.setState(st);
+        }) 
+        
+      })
+      
+      }
      render(){
          let self = this
          console.log(self.state);
