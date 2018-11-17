e629d9838d8d0c23271fc5d72e95f99fe52ee421
bachlekim <lekimbach0912@gmail.com>
2018-11-16 23:29:35 -0800

    Added get top players by fund, get top players by investments and get networth of a player

--- BackEnd/router/helpers.js
+++ BackEnd/router/helpers.js
@@ -5,7 +5,7 @@ var client = new pg.Client(process.env.CONNECTIONSTR);
 client.connect();
 
 module.exports = {
-    createTables: function() {
+    createTables: function () {
         let price = `CREATE TABLE IF NOT EXISTS price(priceID VARCHAR(10) NOT NULL PRIMARY KEY, pDate VARCHAR(100), value REAL, changePercent REAL)`;
         let company = `CREATE TABLE IF NOT EXISTS company(companyID VARCHAR(6) NOT NULL PRIMARY KEY, numOfShares INTEGER, industry VARCHAR(100), companyName VARCHAR(200), priceID VARCHAR(10) NOT NULL, FOREIGN KEY (priceID) REFERENCES price(priceID))`;
         let leaderBoard = `CREATE TABLE IF NOT EXISTS leaderboard(leaderboardID VARCHAR(10) NOT NULL PRIMARY KEY, numOfTraders INTEGER)`;
@@ -15,9 +15,9 @@ module.exports = {
         let includes = `CREATE TABLE IF NOT EXISTS includes(watchlistID VARCHAR(10) NOT NULL, companyID VARCHAR(6) NOT NULL, PRIMARY KEY (companyID, watchlistID), FOREIGN KEY (watchlistID) REFERENCES watchlist(watchlistID), FOREIGN KEY (companyID) REFERENCES company(companyID))`;
         let contains = `CREATE TABLE IF NOT EXISTS contains(portfolioID VARCHAR(10) NOT NULL, companyID VARCHAR(6), shares INTEGER DEFAULT 0, PRIMARY KEY (portfolioID, companyID), FOREIGN KEY (portfolioID) REFERENCES portfolio(portfolioID), FOREIGN KEY (companyID) REFERENCES company(companyID))`;
         let transaction = `CREATE TABLE IF NOT EXISTS transaction(transactionID VARCHAR(10) PRIMARY KEY, traderID VARCHAR(10) NOT NULL, companyID VARCHAR(6) NOT NULL, priceID VARCHAR(10) NOT NULL, type BIT(1), sharesPurchased INTEGER, FOREIGN KEY (traderID) REFERENCES trader(traderID) ON DELETE NO ACTION ON UPDATE CASCADE, FOREIGN KEY (priceID) REFERENCES price(priceID) ON DELETE NO ACTION ON UPDATE CASCADE)`;
-        
+
         let arr = [price, company, leaderBoard, portfolio, trader, watchList, includes, contains, transaction];
-        
+
         arr.forEach((query) => {
             client.query(query, (err, result) => {
                 if (err) {
@@ -27,7 +27,7 @@ module.exports = {
         });
     },
 
-    createLeaderboard: function() {
+    createLeaderboard: function () {
         let leaderboardID = 1;
         let addLeaderboard = `INSERT INTO leaderboard(leaderboardID, numOfTraders) values ($1, $2)`
         client.query(addLeaderboard, [leaderboardID, 0], (err, result) => {
@@ -37,7 +37,7 @@ module.exports = {
         });
     },
 
-    getCompanyByID: async function(CID) {
+    getCompanyByID: async function (CID) {
         try {
             let findCompany = `SELECT * FROM company WHERE companyid = $1`;
             let company = await client.query(findCompany, [CID]);
@@ -47,7 +47,7 @@ module.exports = {
         }
     },
 
-    addTransaction: async function(TID, CID, priceID, type, numOfShares) {
+    addTransaction: async function (TID, CID, priceID, type, numOfShares) {
         try {
             let TXID = generateID();
             let addTX = `INSERT INTO transaction(transactionID, traderID, companyID, priceID, type, sharesPurchased) values($1, $2, $3, $4, $5, $6)`;
@@ -58,7 +58,7 @@ module.exports = {
         }
     },
 
-    getValue: async function(priceID) {
+    getValue: async function (priceID) {
         try {
             let findPrice = `SELECT * FROM price WHERE priceid = $1`;
             let price = await client.query(findPrice, [priceID]);
@@ -68,7 +68,7 @@ module.exports = {
         }
     },
 
-    updateFunds: async function(TID, value, numOfShares, type) {
+    updateFunds: async function (TID, value, numOfShares, type) {
         try {
             let findFunds = `SELECT funds FROM trader WHERE traderID = $1`;
             let funds = await client.query(findFunds, [TID]);
@@ -86,7 +86,7 @@ module.exports = {
         }
     },
 
-    getPortfolioID: async function(TID) {
+    getPortfolioID: async function (TID) {
         try {
             let getPortID = `SELECT portfolioID FROM trader WHERE traderID = $1`;
             let rows = await client.query(getPortID, [TID]);
@@ -97,7 +97,7 @@ module.exports = {
         }
     },
 
-    checkContains: async function(portfolioID, CID, type, numOfShares) {
+    checkContains: async function (portfolioID, CID, type, numOfShares) {
         try {
             let join = `SELECT * FROM contains WHERE portfolioID = $1 AND companyID = $2`;
             let companys = await client.query(join, [portfolioID, CID]);
@@ -150,22 +150,22 @@ module.exports = {
         })
     },
 
-      getAPI: function (url) {
-        return new Promise(function(resolve, reject) {
+    getAPI: function (url) {
+        return new Promise(function (resolve, reject) {
             request({
-              url: url,
-              method: 'GET'
-            },function(err, response, body) {
-              if (err) {
-                console.log("ERROR: " + err);
-              } else {
-                resolve(body);
-              }
+                url: url,
+                method: 'GET'
+            }, function (err, response, body) {
+                if (err) {
+                    console.log("ERROR: " + err);
+                } else {
+                    resolve(body);
+                }
             });
-          })
-      },
+        })
+    },
 
-      getPriceIds: function (priceid, amountofshares) {
+    getPriceIds: function (priceid, amountofshares) {
         return new Promise(function (resolve, reject) {
             let findVal = `SELECT value, priceID FROM price WHERE priceID = $1`;
             let eachValue = [];
@@ -200,7 +200,7 @@ module.exports = {
         }
     },
 
-    addCompany: async function(symbol, json, industry) {
+    addCompany: async function (symbol, json, industry) {
         let name = json.companyName;
         let price = json.latestPrice;
         let volume = json.latestVolume;
@@ -219,5 +219,30 @@ module.exports = {
                 await client.query(updateCompany, [id, volume, symbol]);
             }
         }
+    },
+
+    getNetWorth: async function (inputname) {
+        let name = inputname;
+        let getPortfolioID = `SELECT portfolioID, funds FROM trader WHERE tradername = $1`;
+        let result1 = await client.query(getPortfolioID, [name]);
+        let portfolioID = result1.rows[0].portfolioid;
+        let funds = result1.rows[0].funds;
+        let companyAndPriceView = `CREATE OR REPLACE VIEW temp(companyID, value) AS SELECT c.companyID, p.value FROM company c, price p WHERE c.priceID = p.priceID`;
+        await client.query(companyAndPriceView);
+        // Gives us companyID, the number of shares this person owns and the price of each share
+        let selectFromView = `SELECT t.companyID, c.shares, t.value FROM temp t, contains c WHERE t.companyID = c.companyID AND portfolioID = $1`;
+        let result3 = await client.query(selectFromView, [portfolioID]);
+        // Loop through each row to calculate the total value of each company and add it together for that person
+        // console.log("ATTENTION" + result3.rows);
+        let arrayOfValues = [];
+        let companiesWorth = 0;
+        for (let company of result3.rows) {
+            arrayOfValues.push(company.shares * company.value);
+        }
+        for (let i of arrayOfValues) {
+            companiesWorth = companiesWorth + i;
+        }
+        let totalWorth = companiesWorth + funds;
+        return totalWorth;
     }
 }
