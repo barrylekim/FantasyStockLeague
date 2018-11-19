2e64ed5ce6addcf7e0fe48f212906cbdb1376d47
rohan garg <rohangarg1998s@gmail.com>
2018-11-18 18:24:41 -0800

    L

--- FrontEnd/src/containers/GameInsights/GameInsights.js
+++ FrontEnd/src/containers/GameInsights/GameInsights.js
@@ -5,10 +5,14 @@ class GameInsights extends Component {
         this.state =  {
             queryShare: "",
             queryFound: false,
-            result: null
+            result: null,
+            showTransactions: false,
+            transactions:[]
         }
         this.handleChange = this.handleChange.bind(this); 
         this.submitData = this.submitData.bind(this); 
+        this.transB = this.transB.bind(this); 
+        this.handleTransactions = this.handleTransactions.bind(this); 
     }
     handleChange(event){
         let st = this.state; 
@@ -19,29 +23,79 @@ class GameInsights extends Component {
         event.preventDefault()
         let st = this.state
         let data = {companyid: st.queryShare}; 
-        fetch('http://localhost:3005/largestShare',{
-            method: 'POST',
-            mode: 'cors',
-            credentials: 'same-origin',
-            headers:{
-                "Content-Type": "application/json; charset=utf-8"
-            },
-            body: JSON.stringify(data)
-        }).then(res =>{
+        fetch('http://localhost:3005/largestShare/'+st.queryShare).then(res =>{
             return res.json();
         }).then(myt=>{
             st.queryFound = true;
-            st.result = myt; 
+            st.result = myt.traderid; 
             this.setState(st);  
             console.log(myt)
         })
+    }
+    transB(){
+        let st = this.state;
+        if(st.showTransactions){
+            st.showTransactions= false; 
+        }else{
+            st.showTransactions=true; 
+            fetch('http://localhost:3005/transaction').then(res =>{
+                return res.json();
+            }).then(myt=>{
+                myt.forEach(element => {
+                    let t = {}; 
+                    t.id = element.transactionid;
+                    t.traderid = element.traderid;
+                    t.comp = element.companyid; 
+                    t.shares = element.sharespurchased; 
+                    st.transactions.push(t); 
+                });
+                this.setState(st); 
+            })
+        }
+    }
+    handleTransactions(param){
+        console.log(
+            param
+        )
+        
+        let st = this.state; 
+        if(param)
+        {let val = (
+            <table>
+                <tr>
+                    <th>Transaction Id</th>
+                    <th>Trader Id</th>
+                    <th>Company</th>
+                    <th>Shares</th>
+                </tr>
+                <tbody>
+                {st.transactions.map((el,index)=>{
+                    return(
+                        <tr>
+                        <td>{el.id}</td>
+                        <td>{el.traderid}</td>
+                        <td>{el.comp}</td>
+                        <td>{el.shares}</td>
+                        </tr>
+                    )
+                })}
+                </tbody>
+            </table>
+        )
+        console.log(val); 
+        return val; }else{
+            return; 
+        }
+
     }
     render(){
         return (<div>
             <form onSubmit={this.submitData}>
         <input onChange={this.handleChange} value={this.state.queryShare}></input>
         <input type="submit" value="Submit"></input></form>
-        {this.state.queryFound ? this.state.result : this.state.queryShare }
+        {this.state.queryFound ? this.state.result : <div>TypeheretoFindThePerson with the higest shraes of company X </div>}
+        <button onClick = {this.transB}>Click Here to See All Transactions</button>
+        {this.handleTransactions(this.state.showTransactions)}
         </div>)
     }
 }
